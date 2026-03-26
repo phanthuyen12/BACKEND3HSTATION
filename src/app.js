9b1149a2a064d766060a81e65adc5ae6c0c01b69
phan gia thuyên <thuyendi2004@gmail.com>
2026-03-27 02:42:07 +0700

    f

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(cors({
-  origin: 'http://localhost:5173', // frontend URL
+  origin: 'http://3hstation.com', // frontend URL
   credentials: true, // nếu dùng cookie
 }));
 app.get('/health', (_req, res) => {
